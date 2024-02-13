6fe05907e5e24f57de5350cc10dbe9eea64ad3f9
kavics <kavics@sensenet.com>
2024-02-13 04:14:16 +0100

    Repositories command (#43)

---
+++ src/GitT/Commands/RepositoriesArguments.cs
@@ -0,0 +1,15 @@
+﻿using SenseNet.Tools.CommandLineArguments;
+
+namespace GitT.Commands;
+
+internal class RepositoriesArguments
+{
+    [NoNameOption(order: 0, required: true, nameInHelp: "Name", helpText: "Name of the organization or user.")]
+    public string OrganizationName { get; set; } = "<<<unknown>>>";
+
+    [CommandLineArgument(aliases: "B", helpText: "Show all branches of every repository.")]
+    public bool Branches { get; set; }
+
+    [CommandLineArgument(aliases: "I", helpText: "Show all issue titles of every repository.")]
+    public bool Issues { get; set; }
+}
--- src/GitT/Program.cs
+++ src/GitT/Program.cs
@@ -1,119 +1,130 @@
 ﻿using GitT.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
-namespace GitT
+namespace GitT;
+
+public class GitToolsOptions
 {
-    internal class Program
-    {
-        private static readonly string[] CommandNames = new string[] {"components", "configure", "status"};
+    public string? GitHubToken { get; set; }
+}
 
-        private static readonly IHost Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
-            .ConfigureServices((context, services) =>
-            {
-                services
-                    .AddSingleton<INugetTools, NugetTools>()
-                    .AddKeyedTransient<ICommand, ComponentsCommand>("components")
-                    .AddKeyedTransient<ICommand, ConfigureCommand>("configure")
-                    .AddKeyedTransient<ICommand, StatusCommand>("status")
-                    ;
+internal class Program
+{
+    private static readonly string[] CommandNames = new string[] {"components", "configure", "status", "repositories" };
 
-            }).Build();
-
-        private static void Main(string[] args)
+    private static readonly IHost Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+        .ConfigureServices((context, services) =>
         {
-            args = new[] { "status" };
-            //args = new[] { "components", "-?" };
-            //args = new[] { "components" };
-            //args = new[] { "components", "-nuget" };
-            //args = new[] { "components", "-refs" };
-            //args = new[] { "components", "-diff" };
-            //args = new[] { "components", "-nuget", "-refs" };
-            //args = new[] { "components", "-nuget", "-diff" };
-            //args = new[] { "components", "-refs", "-diff" };
-            //args = new[] { "components", "-nuget", "-refs", "-diff" };
-
-            var githubContainer = Directory.GetCurrentDirectory();
-            githubContainer = @"D:\dev\github";
-            //githubContainer = @"D:\dev\github\sensenet";
-
-            Run(githubContainer, args);
-        }
-
-        private static void Run(string githubContainer, string[] args)
-        {
-            var command = GetCommand(args.FirstOrDefault());
-            if (command == null)
-                return;
-
-            try
-            {
-                var context = new CommandContext(githubContainer, command, args.Skip(1).ToArray());
-                if (!(command is ConfigureCommand) && context.Config.GitExePath == null)
+            services
+                .AddSingleton<IGitHubTools, GithubTools>()
+                .AddSingleton<INugetTools, NugetTools>()
+                .AddKeyedTransient<ICommand, ComponentsCommand>("components")
+                .AddKeyedTransient<ICommand, ConfigureCommand>("configure")
+                .AddKeyedTransient<ICommand, StatusCommand>("status")
+                .AddKeyedTransient<ICommand, RepositoriesCommand>("repositories")
+                .Configure<GitToolsOptions>(opt =>
                 {
-                    Console.WriteLine("GitT cannot run because git.exe was not found.");
-                    Console.WriteLine("Please configure the full path of the git.exe with the following command:");
-                    Console.WriteLine("GitT Configure GitExe <fullpath>");
+                    context.Configuration.GetSection("gitTools").Bind(opt);
+                });
 
-                    return;
-                }
-                command.Context = context;
-                command.Execute();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
+        }).Build();
 
-        private static readonly string[] HelpStrings = { "/?", "/h", "/help", "-?", "-h", "-help", "--help" };
-        internal static ICommand? GetCommand(string? commandName)
+    private static void Main(string[] args)
+    {
+        //args = new[] { "status", "-?" };
+        //args = new[] { "status" };
+        //args = new[] { "status", "-fetch" };
+        //args = new[] { "components", "-?" };
+        //args = new[] { "components" };
+        //args = new[] { "components", "-nuget" };
+        //args = new[] { "components", "-refs" };
+        //args = new[] { "components", "-diff" };
+        //args = new[] { "repositories", "sensenet" };
+        //args = new[] { "repositories", "kavics", "-branches" };
+        //args = new[] { "repositories", "kavics", "-issues" };
+
+        var githubContainer = Directory.GetCurrentDirectory();
+        //githubContainer = @"D:\dev\github";
+        //githubContainer = @"D:\dev\github\sensenet";
+
+        Run(githubContainer, args);
+    }
+
+    private static void Run(string githubContainer, string[] args)
+    {
+        var command = GetCommand(args.FirstOrDefault());
+        if (command == null)
+            return;
+
+        try
         {
-            if (commandName == null)
-                commandName = "Status";
-            else if (HelpStrings.Contains(commandName.ToLowerInvariant()))
+            var context = new CommandContext(githubContainer, command, args.Skip(1).ToArray());
+            if (!(command is ConfigureCommand) && context.Config.GitExePath == null)
             {
-                WriteHelp();
-                return null;
+                Console.WriteLine("GitT cannot run because git.exe was not found.");
+                Console.WriteLine("Please configure the full path of the git.exe with the following command:");
+                Console.WriteLine("GitT Configure GitExe <fullpath>");
+
+                return;
             }
+            command.Context = context;
+            command.Execute();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
 
-            var command = Host.Services.GetKeyedService<ICommand>(commandName.ToLowerInvariant());
-            if (command != null)
-                return command;
-
-            WriteError($"Unknown command: {commandName}\r\n{GetAvailableCommandsMessage()}");
+    private static readonly string[] HelpStrings = { "/?", "/h", "/help", "-?", "-h", "-help", "--help" };
+    internal static ICommand? GetCommand(string? commandName)
+    {
+        if (commandName == null)
+            commandName = "Status";
+        else if (HelpStrings.Contains(commandName.ToLowerInvariant()))
+        {
+            WriteHelp();
             return null;
         }
 
-        private static void WriteError(string message)
-        {
-            Console.WriteLine(message);
-        }
+        var command = Host.Services.GetKeyedService<ICommand>(commandName.ToLowerInvariant());
+        if (command != null)
+            return command;
 
-        private static string GetAvailableCommandsMessage() =>
-            "Available commands:\r\n  " + string.Join("\r\n  ", CommandNames);
+        WriteError($"Unknown command: {commandName}\r\n{GetAvailableCommandsMessage()}");
+        return null;
+    }
 
-        private static void WriteHelp()
-        {
+    private static void WriteError(string message)
+    {
+        Console.WriteLine(message);
+    }
 
-            var message = "GitT </? | -? | /h | -h | /help | -help | --help>\r\n" +
-                          "GitT <command> [command-arguments]\r\n" +
-                          "GitT <command> </? | -? | /h | -h | /help | -help | --help>\r\n" +
-                          "\r\n" + GetAvailableCommandsMessage();
+    private static string GetAvailableCommandsMessage() =>
+        "Available commands:\r\n  " + string.Join("\r\n  ", CommandNames);
 
-            Usage(message);
+    private static void WriteHelp()
+    {
 
-        }
+        var message = "GitT </? | -? | /h | -h | /help | -help | --help>\r\n" +
+                      "GitT <command> [command-arguments]\r\n" +
+                      "GitT <command> </? | -? | /h | -h | /help | -help | --help>\r\n" +
+                      "\r\n" + GetAvailableCommandsMessage();
 
-        private static void Usage(string message)
-        {
-            Console.WriteLine();
-            Console.WriteLine("Git Tools V0.2");
-            Console.WriteLine("==============");
-            Console.WriteLine();
-            Console.WriteLine("Usage:");
-            Console.WriteLine(message);
-            Console.WriteLine();
-        }
+        Usage(message);
+
+    }
+
+    private static void Usage(string message)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Git Tools V0.2");
+        Console.WriteLine("==============");
+        Console.WriteLine();
+        Console.WriteLine("Usage:");
+        Console.WriteLine(message);
+        Console.WriteLine();
     }
 }
